6ff9918f6421a1203f3aff1de3fc1411f1ed1420
John Azzaro <jazzaro@temple.edu>
2018-10-13 10:19:48 -0700

    JavaScript improvements

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories';
 
-function getDataFromApi(searchTerm, callback) {
+function getDataFromApi(searchTerm, callback) {  // gas station analogy
   const query = {
     q: `${searchTerm} in:name`,
     per_page: 4
@@ -22,26 +22,27 @@ function renderResult(result) {
 }
 
 function displayGitHubSearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item));
+  const results = data.items.map((item) => renderResult(item));
   $('.js-search-results').html(results);
 }
 
-function watchSubmit() {
+function handleSubmit() {
   $('.js-search-form').submit(function(event) {
     event.preventDefault();
-    const queryTarget = $(event.currentTarget).find('.js-query');
-    const query = queryTarget.val();
+    const userInputTextBox = $(event.currentTarget).find('.js-query');   // event.curent target is the form itself
+    const userInput = userInputTextBox.val();
     queryTarget.val("");
-    getDataFromApi(query, displayGitHubSearchData);
+    getDataFromApi(userInput, displayGitHubSearchData);   // the name doesnt matter, the slot is the important part.  the first slot (user input) is passed to get data api as searchterm, secod hole is the callback hole. picture it as a cone with holes, and 
+    // when we pass the paramters to the getdataapi function, it will pass to searchterm and named differently
   });
 }
 
 function setUpEventHandlers() {
-  watchSubmit();
+  handleSubmit();
 }
 
 function initializeApp() {
   setUpEventHandlers()
 }
 
-$(initializeApp)
+$(initializeApp);
