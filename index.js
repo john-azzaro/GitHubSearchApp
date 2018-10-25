829dcb1b61d0577348aaa4ab69ded58c9db66ab8
John Azzaro <jazzaro@temple.edu>
2018-10-24 21:05:20 -0700

    fixed javascript

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+// GitHub search app with start and app page
+
 "use strict";
 
 const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories';
 
-function getDataFromApi(searchTerm, callback) {  // gas station analogy
+function getDataFromApi(searchTerm, callback) {
   const query = {
     q: `${searchTerm} in:name`,
-    per_page: 4
+    per_page: 3
   }
   $.getJSON(GITHUB_SEARCH_URL, query, callback);
 }
 
 function renderResult(result) {
   return `
-    <div class="resultStyle">
+    <div>
       <h2>
       <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
       <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
@@ -27,22 +29,87 @@ function displayGitHubSearchData(data) {
 }
 
 function handleSubmit() {
-  $('.js-search-form').submit(function(event) {
+  $('main').submit(function(event) {
     event.preventDefault();
-    const userInputTextBox = $(event.currentTarget).find('.js-query');   // event.curent target is the form itself
-    const userInput = userInputTextBox.val();
+    // event.curent target is the form itself
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
     queryTarget.val("");
-    getDataFromApi(userInput, displayGitHubSearchData);   // the name doesnt matter, the slot is the important part.  the first slot (user input) is passed to get data api as searchterm, secod hole is the callback hole. picture it as a cone with holes, and 
+    // the name doesnt matter, the slot is the important part.  the first slot (user input) is passed to get data api as searchterm, secod hole is the callback hole. picture it as a cone with holes, and 
     // when we pass the paramters to the getdataapi function, it will pass to searchterm and named differently
+    getDataFromApi(query, displayGitHubSearchData);
   });
 }
 
-function setUpEventHandlers() {
-  handleSubmit();
+
+/////////
+
+function generateStartHtml() {
+    return `
+    <h4>Passing Information Between Functions</h4>
+    <p>In this study, you'll find a simple app that demonstrates how to pass information between functions.</p>
+    <p>The study app will go as follows:</p>
+    <ul>
+        <li>Use the GitHub Search App as the working model</li>
+        <li>Observe how information is passed between function within this model</li>
+        <li>Break down and understand how the code works in preceeding steps</li>
+    </ul>
+    <p>Click through to go to the app</p>
+    <button id=startApp>Start!</button>
+    `
 }
+
+function generateAppHtml() {
+    return `
+    <h4>Passing Information Between Functions</h4>
+    <h1>Repository Search on GitHub</h1>
+    <form action="#" class="js-search-form">
+        <label for="query">Search repositories: </label>
+        <input type="text" class="js-query" placeholder="e.g., John-Azzaro">
+        <button type="submit">Search</button>
+    </form>
+    <h2>Results</h2>
+    <div class="js-search-results">
+        <p>search results go here</p>
+    </div>
+    <button id="goback">Go Back</button>
+    `
+}
+
+function renderStartPage() {
+    const startHtml = generateStartHtml();
+    $('main').html(startHtml);
+}
+
+
+function renderAppPage() {
+    const appHtml = generateAppHtml();
+    $('main').html(appHtml);
+}
+
+function handleStartApp() {
+    $('main').on('click', '#startApp', function(event) {
+        renderAppPage();
+    });
+}
+
+function handleGoBack() {
+    $('main').on('click', '#goback', function(event) {
+        renderStartPage();
+    });
+}
+
+function setUpEventHandlers() {
+    handleStartApp();
+    handleGoBack();
+    handleSubmit();
+}
+
 
 function initializeApp() {
-  setUpEventHandlers()
+    setUpEventHandlers();
+    renderStartPage();
 }
 
-$(initializeApp);
+$(initializeApp)
